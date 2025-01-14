c6f0587e09992d7d9418acbe2e60a1a42e3f091c
dvh <177888353+dvhlearning@users.noreply.github.com>
2025-01-14 11:51:41 -0500

    created a button to switch between color and opacity mode. Opacity mode only works on colored squares now. Might want to tweak it a bit still

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
 const gridContainer = document.querySelector(".gridContainer");
 const sizeButton = document.querySelector(".sizeButton");
 const resetButton = document.querySelector(".resetButton");
+const modeButton = document.querySelector(".modeButton");
+
 const inkColor = "blue";
 
 let gridDivs = document.querySelectorAll(".gridDiv");
 let size = document.querySelector(".size");
+let mode = document.querySelector(".mode");
+let modeText = document.querySelector(".modeText");
 
 
-function getNewSize() {
+function setNewSize() {
     const sNum = prompt("Please enter a new size (1-100):");
     let testNum = Number(sNum);
 
@@ -17,40 +21,51 @@ function getNewSize() {
             return sNum;
         } else {
             console.log(`ERROR: ${testNum} is out of range`);
-            return getNewSize();
+            return setNewSize();
         }
     } else {
         console.log(`ERROR: ${testNum} is not a number`);
-        return getNewSize();
+        return setNewSize();
     }
 }
+
+
+function addDrawing(e) {
+    e.target.style.backgroundColor = inkColor;
+}
+
+function addOpacity(e) {
+    //will only work on the color squares
+    if (e.target.style.backgroundColor === 'blue'){
+        if (e.target.style.opacity){
+            e.target.style.opacity = e.target.style.opacity - 0.1;
+        } else {
+            e.target.style.opacity = 0.9;
+        }
+    }
+}
+
 function createGrid(gridTotal) {
     gridContainer.innerHTML = '';
     for (let i = 0; i < gridTotal; i++) {
         const div = document.createElement("div");
         div.classList.add("gridDiv");
-        div.addEventListener("mouseenter", (e) => {
-            console.log(`entered ${e.target}`);
-            //e.target.style.backgroundColor = inkColor;
-            if (e.target.style.opacity){
-                e.target.style.opacity = e.target.style.opacity - 0.1;
-            } else {
-                e.target.style.opacity = 0.9;
-            }
+        if(mode.textContent === 'Color'){
+            div.addEventListener("mouseenter", addDrawing);
+        } else if (mode.textContent === 'Opacity') {
+            div.addEventListener("mouseenter", addOpacity);
+        }
 
-        });
         gridContainer.appendChild(div);
     }
     gridDivs = document.querySelectorAll(".gridDiv");
 }
 
 sizeButton.addEventListener("click", () => {
-    const sizeNum = getNewSize();
-    console.log(`1. sizeNum = ${sizeNum}`);
+    const sizeNum = setNewSize();
     const newDimension = (960 / sizeNum) - 2;
-    console.log(`2. newDimension = ${newDimension}`);
     const newGridTotal = sizeNum * sizeNum;
-    console.log(`3. newGridTotal = ${newGridTotal}`);
+
     size.textContent = `${sizeNum}x${sizeNum}`;
 
     createGrid(newGridTotal);
@@ -67,6 +82,25 @@ resetButton.addEventListener("click", () => {
         gridDiv.style.backgroundColor = "white";
         gridDiv.style.opacity = 1.0;
     });
+})
+
+modeButton.addEventListener("click", () => {
+    if(mode.textContent === 'Color'){
+        mode.textContent = 'Opacity';
+        modeText.textContent = 'Color';
+        gridDivs.forEach((div) => {
+            div.removeEventListener("mouseenter", addDrawing);
+            div.addEventListener("mouseenter", addOpacity);
+        });
+    } else if (mode.textContent === 'Opacity') {
+        mode.textContent = 'Color';
+        modeText.textContent = 'Opacity';
+        gridDivs.forEach((div) => {
+            div.removeEventListener("mouseenter", addOpacity);
+            div.addEventListener("mouseenter", addDrawing);
+        });
+    }
+
 })
 
 createGrid(256);
